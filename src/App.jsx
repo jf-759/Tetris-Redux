6387aa86b486b52f3391b4a2fdc4ff8739f56e2a
jf-759 <jane.fernandez@students.dominican.edu>
2025-06-11 10:05:45 -0700

    completed 01-Grid-Square.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import GridSquare from './components/GridSquare.jsx';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Tetris Redux</h1>
+      </header>
+      <GridSquare color="1" />
+    </div>
+  );
+}
+
+export default App;
